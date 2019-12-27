17e1cb4739b4c8e2c3766ad93c10cdbcfe642b31
skyliqf <liqifang1003@gmail.com>
2019-12-27 14:47:36 +0800

    it is a vector demo

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -3,7 +3,22 @@
 #include<string.h>
 using namespace std;
 int main(){
-
+   vector<int>obj;
+   for(int i=0;i<8;i++){
+     obj.push_back(i);
+   }
+   for(int i=0;i<8;i++){
+     cout<<obj[i]<<" ,";
+   }
+   cout<<"\n"<<endl;
+   for(int i=0;i<5;i++){
+     obj.pop_back();
+   }
+   cout<<"\n";
+   for(int i=0;i<obj.size();i++){
+     cout<<obj[i]<<", ";
+   }
+   cout<<endl;
    cout<<"this is a vector test"<<endl;
    return 0;
 }
